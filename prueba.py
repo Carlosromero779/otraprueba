55da41f9594afceb4483c3bfc08808790b7556fa
Carlos Romero <carlosromeros779@gmail.com>
2026-03-22 13:25:52 -0400

    prueba de conflictos de rama

--- prueba.py
+++ prueba.py
@@ -3,3 +3,10 @@ print("Hello, World!")
 def saludar(nombre):
     return f"Hola, {nombre}!"
 nombre_usuario = "Alice"
+
+def main():
+    mensaje = saludar(nombre_usuario)
+    print(mensaje)
+
+if __name__ == "__main__":    main()
+"para observar los conflictos de merge"
